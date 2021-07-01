ba13f863a1efd8b12be82e2bb041d2cd0f731c99
seungyong88 <gosng2001@naver.com>
2021-07-01 11:04:44 +0900

    TodoList Styling

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="index">{{todoItem}}</li>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="index" class="shadow">
+        {{todoItem}}
+        <span class="removeBtn">
+          <i class="fas fa-trash"></i>
+        </span>
+      </li>
     </ul>
   </div>
 </template>
@@ -20,11 +25,50 @@ export default {
         }
       }
     }
-
-    console.log(this.todoItems);
-  }
+  },
+  methods: {
+     removeTodo: function () {
+       
+     }
+  },
 }
 </script>
-<style>
-  
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right:5px;
+  }
+
+  .checkBtnCompleted {
+    color: #b3adad;
+  }
+
+  .textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+  }
+
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
 </style>
