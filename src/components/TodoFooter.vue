784d7e42926d1ea27df1b95e057da8a162f32f77
seungyong88 <gosng2001@naver.com>
2021-07-01 14:16:16 +0900

    Footer clear 완성

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
-    footer
+  <div class="clearAllContainer" @click="clearTodo">
+    <span class="clearAllBtn">Clear All</span>
   </div>
 </template>
 <script>
 export default {
-  
+  methods: {
+    clearTodo: function() {
+      localStorage.clear();
+    }
+  }
 }
 </script>
-<style>
-  
+<style scoped>
+  .clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+  }
+
+  .clearAllBtn {
+    color: #e20303;
+    display: block;
+  }
 </style>
