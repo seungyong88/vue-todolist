4e688d6c70fb9caed234051099b21e80e197a802
seungyong88 <gosng2001@naver.com>
2021-07-01 10:47:28 +0900

    sync localStorage

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    List
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="index">{{todoItem}}</li>
+    </ul>
   </div>
 </template>
 <script>
 export default {
-  
+  data: function() {
+    return {
+      todoItems: [],
+    }
+  },
+  created() {
+    if(localStorage.length > 0) {
+      for(var i =0; i < localStorage.length; i++) {
+        if(localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+
+    console.log(this.todoItems);
+  }
 }
 </script>
 <style>
